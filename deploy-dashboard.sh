a727b014f2a5c8e6545ddde0f1d58c24131c526e
daewood <daewood@qq.com>
2017-11-06 12:40:50 +0000

    dashboard2

--- deploy-dashboard.sh
+++ deploy-dashboard.sh
@@ -2,14 +2,14 @@
 
 #kubectl create -f https://git.io/kube-dashboard
 export ADDON_URL="https://github.com/daewood/kubernetes/raw/master/addon"
-wget ${ADDON_URL}/kube-dashboard.yml.conf -O kube-dashboard.yml
+wget ${ADDON_URL}/kube-dashboard.yml
 kubectl apply -f kube-dashboard.yml
 #kubectl -n kube-system get po,svc -l k8s-app=kubernetes-dashboard
 
 #echo https://192.168.56.50:6443/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
 
 #export ADDON_URL="https://github.com/daewood/kubernetes/raw/master/addon"
-wget ${ADDON_URL}/kube-monitor.yml.conf -O kube-monitor.yml
+wget ${ADDON_URL}/kube-monitor.yml
 kubectl apply -f kube-monitor.yml
 #kubectl -n kube-system get po,svc
 
