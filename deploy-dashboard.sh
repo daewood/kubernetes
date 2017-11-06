645ea2f8e450d3399b60a34464a48026d705627f
daewood <daewood@qq.com>
2017-11-06 12:47:09 +0000

    dashboard3

--- deploy-dashboard.sh
+++ deploy-dashboard.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 #kubectl create -f https://git.io/kube-dashboard
+
+mkdir -p /etc/kubernetes/addons/certs && cd /etc/kubernetes/addons
+openssl genrsa -des3 -passout pass:x -out certs/dashboard.pass.key 2048
+openssl rsa -passin pass:x -in certs/dashboard.pass.key -out certs/dashboard.key
+openssl req -new -key certs/dashboard.key -out certs/dashboard.csr -subj '/CN=kube-dashboard'
+openssl x509 -req -sha256 -days 365 -in certs/dashboard.csr -signkey certs/dashboard.key -out certs/dashboard.crt
+rm certs/dashboard.pass.key
+kubectl create secret generic kubernetes-dashboard-certs --from-file=certs -n kube-system
+
 export ADDON_URL="https://github.com/daewood/kubernetes/raw/master/addon"
 wget ${ADDON_URL}/kube-dashboard.yml
 kubectl apply -f kube-dashboard.yml
